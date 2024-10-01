22996abe094871eed2c880928760cb16a73a28fc
prathmesh-jagtap <jagtaprathmesh19@gmail.com>
2024-10-01 22:53:30 +0530

    Prop and tailwind

---
+++ tailwind02withreactprops/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+     <Card name="Prathmesh Jagtap" location="India"/>
+     <br />
+     <Card name= "Elon Musk" location="U.S" />
+     
+    </>
+
+  )
+}
+
+export default App
